a17613b4b029bb152248d75ad207b82bcc646c17
Boris <anas.essaghir@gmail.com>
2020-04-30 15:23:39 +0200

    Updates

---
+++ precache-manifest.94283bd0ddaf419cf95e9f1d0fe72681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bdc95a37e1bd2d039e47250a768cca",
+    "url": "/ps-react-ae2s/index.html"
+  },
+  {
+    "revision": "fcf19ee0137666fb6663",
+    "url": "/ps-react-ae2s/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "bdcdbceb5d03960b19d4",
+    "url": "/ps-react-ae2s/static/css/main.5516891c.chunk.css"
+  },
+  {
+    "revision": "fcf19ee0137666fb6663",
+    "url": "/ps-react-ae2s/static/js/2.2e16a7f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ps-react-ae2s/static/js/2.2e16a7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcdbceb5d03960b19d4",
+    "url": "/ps-react-ae2s/static/js/main.00afd8b1.chunk.js"
+  },
+  {
+    "revision": "e3e12a99228b9ad4fcaa",
+    "url": "/ps-react-ae2s/static/js/runtime-main.4be89089.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22ca98107082af6da02c4a9e9e272432.js"
+  "/ps-react-ae2s/precache-manifest.94283bd0ddaf419cf95e9f1d0fe72681.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ps-react-ae2s/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
